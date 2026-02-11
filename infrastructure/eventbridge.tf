25e3bbbfe1371ed7a11d9bb74e1237ef624aca03
Vidyasri-38 <vidyasrinekuri38@gmail.com>
2026-02-11 19:36:16 +0530

    Create eventbridge.tf

---
+++ infrastructure/eventbridge.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_event_rule" "daily_trigger" {
+  name                = "${var.project_name}-daily-trigger"
+  schedule_expression = "cron(0 0 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_trigger.name
+  target_id = "LambdaTarget"
+  arn       = aws_lambda_function.processor.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
+}
