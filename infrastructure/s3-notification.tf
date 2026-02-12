80f9117415b6f4c85b2235bbd8fd1ce4e0b4edce
Vidyasri-38 <vidyasrinekuri38@gmail.com>
2026-02-13 02:55:33 +0530

    Create s3-notification.tf

---
+++ infrastructure/s3-notification.tf
@@ -0,0 +1,22 @@
+# Allow S3 to invoke Lambda
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_data.arn
+}
+
+# S3 Event Notification
+resource "aws_s3_bucket_notification" "raw_trigger" {
+  bucket = aws_s3_bucket.raw_data.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [
+    aws_lambda_permission.allow_s3
+  ]
+}
