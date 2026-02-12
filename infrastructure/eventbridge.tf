3a3c8c6596d7f8062e4d940b2d0c2895cbcd56ab
Vidyasri-38 <vidyasrinekuri38@gmail.com>
2026-02-12 08:12:21 +0530

    Update eventbridge.tf

--- infrastructure/eventbridge.tf
+++ infrastructure/eventbridge.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_rule" "daily_trigger" {
   name                = "${var.project_name}-daily-trigger"
-  schedule_expression = "cron(5 18 * * ? *)"
+  schedule_expression = "cron(45 2 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
